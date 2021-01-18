657bec21f9c5b1f14e30237bb1e0d19e84e9d5f8
NitroFray <32951247+NitroFray@users.noreply.github.com>
2021-01-18 16:51:31 +0100

    Added thread

---
+++ server/ClientThread.java
@@ -0,0 +1,47 @@
+import java.net.*;
+import java.io.*;
+import java.util.Scanner;
+
+public class ClientThread {
+
+    private Socket socket;
+    private ServerActual server;
+    private PrintWriter clientOut;
+
+    public ClientThread(ServerActual server, Socket socket) {
+        this.server = server;
+        this.socket = socket;
+    }
+
+    private PrintWriter getWriter(){
+        return clientOut;
+    }
+
+    //@Override
+    public void run() {
+        try{
+            // setup
+            this.clientOut = new PrintWriter(socket.getOutputStream(), false);
+            Scanner in = new Scanner(socket.getInputStream());
+
+            // start communicating
+            while(!socket.isClosed()){
+                if(in.hasNextLine()){
+                    String input = in.nextLine();
+                    // NOTE: if you want to check server can read input, uncomment next line and check server file console.
+                    System.out.println(input);
+                    for(ClientThread thatClient : server.getClients()){
+                        PrintWriter thatClientOut = thatClient.getWriter();
+                        if(thatClientOut != null){
+                            thatClientOut.write(input + "\r\n");
+                            thatClientOut.flush();
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
--- server/ServerActual.java
+++ server/ServerActual.java
@@ -1,27 +1,65 @@
 import java.net.*;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ServerActual {
 
     static final int portNumber = 42069;
+    private boolean running = false;
+    ServerSocket ss = null;
+
+    private List<ClientThread> clients;
 
     public ServerActual() {
+        clients = new ArrayList<ClientThread>();
         try {
-            ServerSocket ss = new ServerSocket(portNumber);
-            Socket cs = ss.accept();
-            PrintWriter out = new PrintWriter(cs.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(cs.getInputStream()));
+            this.ss = new ServerSocket(portNumber, 0, InetAddress.getLocalHost());
+            System.out.println("Running server on port: "+ss.getLocalPort());
+            acceptClients(ss);
         } catch (IOException e) {
-            e.printStackTrace();
+            System.err.println("Could not listen on port: "+portNumber);
+            System.exit(1);
+        }
+    }
+
+    public List<ClientThread> getClients() {
+        return clients;
+    }
+
+    public void testSocket(Socket s) {
+        while(running) {
+            if (s.isConnected()) {
+                System.out.println("Socket is connected");
+                break;
+            }
         }
     }
 
     public void start() {
-
+        running = true;
     }
 
     public void stop() {
+        running = false;
+    }
 
+    private void acceptClients(ServerSocket serverSocket) {
+        while (true) {
+            try {
+                Socket socket = serverSocket.accept();
+                //System.out.println("accepts : " + socket.getRemoteSocketAddress());
+                System.out.println("Client connected!");
+
+                ClientThread client = new ClientThread(this, socket);
+                Thread thread = new Thread(String.valueOf(client));
+                thread.start();
+                clients.add(client);
+
+            } catch (IOException ex) {
+                System.out.println("Accept failed on : "+portNumber);
+            }
+        }
     }
 
 }
